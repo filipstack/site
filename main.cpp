ba5e90bca046a8244d7a5f53c38ac00c9f675b9c
Filip <filip.alexandru.calin@gmail.com>
2025-12-26 22:39:58 +0200

    Createed main.cpp
    This is an optional commit description that i may or may not have filled in.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+int x;
+int array[]={1, 2, 3, 4, 5, 6};
+
+int main() 
+{
+	printf(array[2]);
+	return 0;
+}
